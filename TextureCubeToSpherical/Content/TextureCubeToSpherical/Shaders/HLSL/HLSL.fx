5345010447383571d4c005a6c5cccb845f4e3d79
jcant0n <javiercantonferrero@outlook.es>
2019-10-04 17:08:07 +0200

    First commit.

---
+++ TextureCubeToSpherical/Content/TextureCubeToSpherical/Shaders/HLSL/HLSL.fx
@@ -0,0 +1,81 @@
+//#define SHOW_NORMALS
+//#define SHOW_FACES
+
+TextureCube cubeTexture 			: register(t0);
+SamplerState Sampler			 	: register(s0);
+
+struct PS_IN
+{
+	float4 Position		: SV_POSITION;
+	float4 PositionCS	: TEXCOORD0;
+};
+
+PS_IN VS(uint id: SV_VertexID)
+{
+	PS_IN output = (PS_IN)0;
+
+	float2 tex = float2((id << 1) & 2, id & 2);
+	output.Position = float4(tex * float2(2, -2) + float2(-1, 1), 0, 1);
+	output.PositionCS = output.Position;
+
+	return output;
+}
+
+inline float2 ComputeScreenPosition(float4 pos)
+{
+	float2 screenPos = pos.xy / pos.w;
+	return (0.5f * (float2(screenPos.x, screenPos.y) + 1));
+}
+
+float4 PS(PS_IN input) : SV_Target
+{
+	float2 screenPosition = ComputeScreenPosition(input.PositionCS);
+	float theta = screenPosition.x * 6.28318;
+	float phi = screenPosition.y * 3.1415926 - 1.57079;
+
+	float3 dir;
+	dir.x = cos(phi) * cos(theta);
+	dir.y = sin(phi);
+	dir.z = cos(phi) * sin(theta);
+	
+	float4 color = cubeTexture.Sample(Sampler, dir);
+
+#ifdef SHOW_NORMALS
+
+	return float4(dir, 1.0);
+
+#else
+
+	#ifdef SHOW_FACES
+		float4 faceColors[6];
+		faceColors[0] = float4(1.0, 0.0, 0.0, 1.0); //left
+		faceColors[1] = float4(0.0, 1.0, 0.0, 1.0); //right
+		faceColors[2] = float4(0.0, 0.0, 1.0, 1.0); //bottom
+		faceColors[3] = float4(1.0, 1.0, 0.0, 1.0); //top
+		faceColors[4] = float4(1.0, 0.0, 1.0, 1.0); //front
+		faceColors[5] = float4(0.0, 1.0, 1.0, 1.0); //back
+
+		float ax = abs(dir.x);
+		float ay = abs(dir.y);
+		float az = abs(dir.z);
+
+		if (ax > ay && ax > az) 
+		{
+			//x-major
+			color *= (dir.x < 0.0 ? faceColors[0] : faceColors[1]);
+		}
+		else if (ay > ax && ay > az) 
+		{
+			//y-major
+			color *= (dir.y < 0.0 ? faceColors[2] : faceColors[3]);
+		}
+		else
+		{
+			//z-major
+			color *= (dir.z < 0.0 ? faceColors[4] : faceColors[5]);
+		}
+	#endif
+
+	return color;
+#endif
+}
---
+++ TextureCubeToSpherical/SphericalProjectionTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using WaveEngine.Common.Graphics;
+using WaveEngine.Mathematics;
+
+namespace TextureCubeToSpherical
+{
+    public class SphericalProjectionTest : BaseTest
+    {
+        private Viewport[] viewports;
+        private Rectangle[] scissors;
+        private CommandQueue graphicsCommandQueue;
+        private GraphicsPipelineState graphicsPipelineState;
+        private ResourceSet resourceSet;
+        private uint width;
+        private uint height;
+
+        public SphericalProjectionTest()
+            : base("TextureCubeToSpherical")
+        {
+        }
+
+        protected override async void InternalLoad()
+        {
+            var swapChainDescription = this.swapChain?.SwapChainDescription;
+            this.width = swapChainDescription.Value.Width;
+            this.height = swapChainDescription.Value.Height;
+
+            // Graphics Resources
+            var vertexShaderDescription = await this.assetsDirectory.ReadAndCompileShader(this.graphicsContext, "HLSL", "VertexShader", ShaderStages.Vertex, "VS");
+            var pixelShaderDescription = await this.assetsDirectory.ReadAndCompileShader(this.graphicsContext, "HLSL", "FragmentShader", ShaderStages.Pixel, "PS");
+            var vertexShader = this.graphicsContext.Factory.CreateShader(ref vertexShaderDescription);
+            var pixelShader = this.graphicsContext.Factory.CreateShader(ref pixelShaderDescription);
+
+            Texture textureCube = null;
+            using (var stream = System.IO.File.OpenRead("Content/TextureCubeToSpherical/TextureCube.ktx"))
+            {
+                if (stream != null)
+                {
+                    VisualTests.LowLevel.Images.Image image = VisualTests.LowLevel.Images.Image.Load(stream);
+                    var textureDescription = image.TextureDescription;
+                    textureCube = graphicsContext.Factory.CreateTexture(image.DataBoxes, ref textureDescription);
+                }
+            }
+
+            var samplerDescription = SamplerStates.LinearClamp;
+            var samplerState = this.graphicsContext.Factory.CreateSamplerState(ref samplerDescription);
+
+            ResourceLayoutDescription layoutDescription = new ResourceLayoutDescription(
+                   new LayoutElementDescription(0, ResourceType.Texture, ShaderStages.Pixel),
+                   new LayoutElementDescription(0, ResourceType.Sampler, ShaderStages.Pixel));
+            ResourceLayout resourceLayout = this.graphicsContext.Factory.CreateResourceLayout(ref layoutDescription);
+
+            ResourceSetDescription resourceSetDescription = new ResourceSetDescription(resourceLayout, textureCube, samplerState);
+            this.resourceSet = this.graphicsContext.Factory.CreateResourceSet(ref resourceSetDescription);
+
+            var pipelineDescription = new GraphicsPipelineDescription()
+            {
+                PrimitiveTopology = PrimitiveTopology.TriangleList,
+                InputLayouts = null,
+                ResourceLayouts = new[] { resourceLayout },
+                Shaders = new ShaderStateDescription()
+                {
+                    VertexShader = vertexShader,
+                    PixelShader = pixelShader,
+                },
+                RenderStates = new RenderStateDescription()
+                {
+                    RasterizerState = RasterizerStates.CullBack,
+                    BlendState = BlendStates.Opaque,
+                    DepthStencilState = DepthStencilStates.Read,
+                },
+                Outputs = this.frameBuffer.OutputDescription,
+            };
+
+            this.graphicsPipelineState = this.graphicsContext.Factory.CreateGraphicsPipeline(ref pipelineDescription);
+            this.graphicsCommandQueue = this.graphicsContext.Factory.CreateCommandQueue(CommandQueueType.Graphics);
+
+            this.viewports = new Viewport[1];
+            this.viewports[0] = new Viewport(0, 0, this.width, this.height);
+            this.scissors = new Rectangle[1];
+            this.scissors[0] = new Rectangle(0, 0, (int)this.width, (int)this.height);
+        }
+
+        protected override void InternalDrawCallback(TimeSpan gameTime)
+        {
+            var graphicsCommandBuffer = this.graphicsCommandQueue.CommandBuffer();
+            graphicsCommandBuffer.Begin();
+
+            RenderPassDescription renderPassDescription = new RenderPassDescription(this.frameBuffer, ClearValue.None);
+            graphicsCommandBuffer.BeginRenderPass(ref renderPassDescription);
+
+            graphicsCommandBuffer.SetViewports(this.viewports);
+            graphicsCommandBuffer.SetScissorRectangles(this.scissors);
+            graphicsCommandBuffer.SetGraphicsPipelineState(this.graphicsPipelineState);
+            graphicsCommandBuffer.SetResourceSet(this.resourceSet);
+            graphicsCommandBuffer.Draw(3);
+
+            graphicsCommandBuffer.EndRenderPass();
+            graphicsCommandBuffer.End();
+
+            graphicsCommandBuffer.Commit();
+
+            this.graphicsCommandQueue.Submit();
+            this.graphicsCommandQueue.WaitIdle();
+        }
+    }
+}
